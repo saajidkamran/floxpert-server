3a6be12869efa882aa646a87ff0f7c0b13fbc7fd
SaajidKamran <saajidkamran@gmail.com>
2023-08-22 23:51:34 +0530

    secured

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const multer = require("multer");
 const cors = require("cors");
-
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const checkAuth = require("./middleware/auth-verify");
 app.use(cors());
 app.use(express.static("./uploads")); // where images and all saved folder
 app.use(express.json());
@@ -36,7 +38,19 @@ const productSchema = new mongoose.Schema({
   category: String,
   image: [],
 });
+const userSchema = mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    match:
+      /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/,
+  },
+  password: { type: String, required: true },
+});
 
+const User = mongoose.model("User", userSchema);
 const Products = mongoose.model("Products", productSchema);
 
 const storage = multer.diskStorage({
@@ -74,7 +88,7 @@ app
       return res.status(500).send(error);
     }
   })
-  .post(upload.array("image", 20), async (req, res, next) => {
+  .post(checkAuth, upload.array("image", 20), async (req, res, next) => {
     try {
       const fileUrls = [];
       req.files.map((image) => {
@@ -109,7 +123,7 @@ app
     }
   });
 
-app.delete("/products/:id", async (req, res) => {
+app.delete("/products/:id", checkAuth, async (req, res, next) => {
   const { id } = req.params;
 
   try {
@@ -120,7 +134,105 @@ app.delete("/products/:id", async (req, res) => {
     res.status(500).send(error);
   }
 });
+app.post("/signup", async (req, res) => {
+  try {
+    const authUser = await User.find({ email: req.body.email }).exec();
+    if (authUser.length >= 1) {
+      return res.status(409).json({
+        error: "user exist",
+      });
+    } else {
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json({
+            error: err,
+          });
+        } else {
+          try {
+            const user = new User({
+              _id: new mongoose.Types.ObjectId(),
+              email: req.body.email,
+              password: hash,
+            });
+            user.save();
+            console.log("user>>", user);
+            res.status(201).json({
+              message: "user created",
+            });
+          } catch (error) {
+            res.status(500).json({
+              error: error,
+            });
+          }
+        }
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      error: error,
+    });
+  }
+});
 
+app.delete("/:userId", (req, res, next) => {
+  User.remove({ _id: req.params.userId })
+    .exec()
+    .then((result) => {
+      res.status(200).json({
+        message: "User deleted",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
+
+app.post("/login", (req, res, next) => {
+  User.find({ email: req.body.email })
+    .exec()
+    .then((user) => {
+      if (user.length < 1) {
+        return res.status(401).json({
+          message: "Auth failed",
+        });
+      }
+      bcrypt.compare(req.body.password, user[0].password, (err, result) => {
+        if (err) {
+          return res.status(401).json({
+            message: "Auth failed",
+          });
+        }
+        if (result) {
+          const token = jwt.sign(
+            {
+              email: user[0].email,
+              userId: user[0]._id,
+            },
+            process.env.JWT_KEY,
+            {
+              expiresIn: "1h",
+            }
+          );
+          return res.status(200).json({
+            message: "Auth successful",
+            token: token,
+          });
+        }
+        res.status(401).json({
+          message: "Auth failed",
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
+});
 app.listen(3000, function () {
   console.log("im on port 3000");
 });
